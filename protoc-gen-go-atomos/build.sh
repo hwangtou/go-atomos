d8888afdb3ebbce0046800277ca7ae4505fb3ba6
hwangtou <tou.hwang@me.com>
2024-12-24 20:23:01 +0800

    feature: Generator with hash

--- protoc-gen-go-atomos/build.sh
+++ protoc-gen-go-atomos/build.sh
@@ -3,4 +3,4 @@ go build -o protoc-gen-go-atomos/protoc-gen-go-atomos protoc-gen-go-atomos/atomo
 GOPATH="/Users/hwangtou/Developer/go"
 
 cp protoc-gen-go-atomos/protoc-gen-go-atomos "$GOPATH"/bin/protoc-gen-go-atomos
-protoc --go_out=. --go-atomos_out=. examples/hello_world/api/hello.proto
+protoc --go_out=. --go-atomos_out=. examples/hello_atomos/api/hello.proto
